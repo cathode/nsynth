066c9019bd564327b336a3ee5ed6142a1fc5b2c2
cathode <cathode@live.com>
2013-08-13 00:20:30 -0700

    added additional boxtype values for iso basemedia

--- source/NSynth/Containers/ISOBaseMediaFormat/BoxTypes.cs
+++ source/NSynth/Containers/ISOBaseMediaFormat/BoxTypes.cs
@@ -14,7 +14,13 @@ namespace NSynth.Containers.ISOBaseMediaFormat
         public const uint Movie = 'm' << 24 | 'o' << 16 | 'o' << 8 | 'v';
         public const uint MovieHeader = 'm' << 24 | 'h' << 16 | 'd' << 8 | 'r';
         public const uint TrackHeader = 't' << 24 | 'k' << 16 | 'h' << 8 | 'd';
-
+        public const uint TrackReference = 0x74725666; // 'tref'
+        public const uint TrackGrouping = 0x74726772; // 'trgr'
+        public const uint EditListContainer = 0x65647473; // 'edts'
+        public const uint EditList = 0x656C7374; // 'elst'
+        public const uint TrackMediaInformationContainer = 0x6D646961; // 'mdia'
+        public const uint MediaHeader = 0x6D646864; // 'mdhd'
+        public const uint MediaHandler = 0x68646c72; // 'hdlr'
         public const uint MediaInformationContainer = 'm' << 24 | 'i' << 16 | 'n' << 8 | 'f';
         public const uint VideoMediaHeader = 'v' << 24 | 'm' << 16 | 'h' << 8 | 'd';
         public const uint SoundMediaHeader = 's' << 24 | 'm' << 16 | 'h' << 8 | 'd';
