37ae7ea1cedcf9bc0299408efe9c8454a9916be2
William Shelley <cathode@live.com>
2014-06-28 01:18:43 -0700

    updated avi codec

--- source/nsynth/Containers/AVI/AviStreamHeader.cs
+++ source/nsynth/Containers/AVI/AviStreamHeader.cs
@@ -11,6 +11,20 @@ namespace NSynth.Containers.AVI
         public AviFourCCType Type;
 
         public uint Handler; // Four-CC of codec contained in stream.
+
+        public AviStreamHeaderFlags Flags;
+
+        public ushort Priority;
+        public ushort Language;
+        public uint InitialFrames;
+        public uint Scale;
+        public uint Rate; // rate / scale = samples per second
+        public uint Start;
+        public uint Length; // in samples
+        public uint SuggestedBufferSize;
+        public uint Quality;
+        public uint SampleSize;
+        public NSynth.Imaging.Rectangle FrameGeometry;
     }
 
     public enum AviFourCCType
@@ -19,4 +33,13 @@ namespace NSynth.Containers.AVI
         Audio,  // 'auds'
         Text,   // 'txts'
     }
+
+    [Flags]
+    public enum AviStreamHeaderFlags
+    {
+        Disabled,               // AVISF_DISABLED
+        VideoPaletteChanges,    // AVISF_VIDEO_PALCHANGES
+
+    }
+
 }
