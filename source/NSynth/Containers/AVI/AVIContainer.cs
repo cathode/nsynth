5e79362fb74ca7903d6ad97f9a6360bbabbe3eee
William Shelley <cathode@live.com>
2014-06-28 13:35:22 -0700

    Added new abstract base Container class.

--- source/NSynth/Containers/AVI/AVIContainer.cs
+++ source/NSynth/Containers/AVI/AVIContainer.cs
@@ -15,69 +15,27 @@ namespace NSynth.Containers.AVI
     /// <summary>
     /// Provides support for demultiplexing multimedia bitstreams contained in an AVI (Audio/Video-Interlaced) container.
     /// </summary>
-    public class AVIContainer : ContainerCodec
+    public class AVIContainer : Container
     {
-        #region Methods
 
-        #endregion
-        #region Properties
-        public override bool CanDecode
-        {
-            get
-            {
-                return true; // Decoding support only for compatibility with older content and programs.
-            }
-        }
-
-        public override bool CanEncode
-        {
-            get
-            {
-                return false; // AVI encoding is not supported and not planned for support.
-            }
-        }
-
-        public override Version Version
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-        #endregion
-
-        public override bool SupportsFrameAccurateSeeking
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        public override bool SupportsNonLinearAccess
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        public override MediaEncoder CreateEncoder(Stream output)
+        public override bool CanContain(Clip clip)
         {
             throw new NotImplementedException();
         }
 
-        public override MediaDecoder CreateDecoder(Stream input)
+        public override Clip GetLayout()
         {
             throw new NotImplementedException();
         }
 
-        public override int MaxThreads
+        public override ContainerStream GetStreamForTrack(int trackId)
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
+        }
+
+        protected override void OpenUnderlyingStream()
+        {
+            throw new NotImplementedException();
         }
     }
 }
